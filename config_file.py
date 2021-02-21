b9e22d75f13fefcf14794d3f1cc10b2bc34c9130
entirelymagic@gmail.com <@lexandr@123>
2021-02-21 19:01:34 +0200

    Added base requirements.

--- config_file.py
+++ config_file.py
@@ -1,12 +1,13 @@
 import configparser
 import os
-import logging.handlers
-
+import json
+import pprint
+from time import sleep
+from BinanceTradingApp.trading_client import BinanceAccountClient
 
 # Config consts
 CFG_FL_NAME = 'data/user.cfg'
 USER_CFG_SECTION = 'binance_user_config'
-
 
 # Init config
 config = configparser.ConfigParser()
@@ -15,20 +16,6 @@ if not os.path.exists(CFG_FL_NAME):
     exit()
 config.read(CFG_FL_NAME)
 
-# Logger setup
-logger = logging.getLogger('crypto_trader_logger')
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh = logging.FileHandler('data/crypto_trading.log')
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(formatter)
-logger.addHandler(fh)
-
-# logging to console
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-ch.setFormatter(formatter)
-logger.addHandler(ch)
 
 # get django secret key from user.cfg
 DJANGO_SECRET_KEY = config.get(USER_CFG_SECTION, 'django_secret_key')
@@ -37,3 +24,27 @@ DJANGO_SECRET_KEY = config.get(USER_CFG_SECTION, 'django_secret_key')
 with open('data/supported_coin_list') as f:
     supported_coin_list = f.read().upper().splitlines()
 
+# Telegram bot
+TELEGRAM_CHAT_ID = config.get(USER_CFG_SECTION, 'botChatID')
+TELEGRAM_TOKEN = config.get(USER_CFG_SECTION, 'botToken')
+BRIDGE = config.get(USER_CFG_SECTION, 'bridge')
+
+
+def main():
+    api_key = config.get(USER_CFG_SECTION, 'api_key')
+    api_secret_key = config.get(USER_CFG_SECTION, 'api_secret_key')
+    binance_acc = BinanceAccountClient(api_key, api_secret_key)
+
+    all_tickers = binance_acc.get_all_tickers()
+
+    print(all_tickers)
+    for item in all_tickers:
+        if item['symbol'] == 'IOTAUSDT':
+            balance = float(binance_acc.get_currency_balance('IOTA'))
+
+            print(float(balance) * float(item['price']))
+            print(balance*1.2)
+
+
+if __name__ == "__main__":
+    main()
